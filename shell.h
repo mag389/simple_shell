6ce1901740f1a5db2f41213c32a2515a7aa595ff
Michael George <mag389@cornell.edu>
2020-04-02 20:41:34 +0000

    adding preliminary header with just inclusions, no prototypes or stuctures

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <string.h>
+
+#endif
